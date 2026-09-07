edb4e9a53f17206ba2da25b434a1460f015d0778
MRX7014 <safen894@gmail.com>
2026-09-07 16:33:19 +0300

    Implement Block Channels feature in YouTube patch
    This patch adds functionality to permanently hide content from specific YouTube channels in various sections of the app.

---
+++ patches/src/main/kotlin/app/morphe/patches/youtube/layout/hide/blockchannels/BlockChannelsPatch.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2026 Morphe.
+ * https://github.com/mrx7014/morphe-patches
+ *
+ * See the included NOTICE file for GPLv3 Section 7 terms that apply to this code.
+ */
+
+package app.morphe.patches.youtube.layout.hide.blockchannels
+
+import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patches.shared.misc.fix.proto.fixProtoLibraryPatch
+import app.morphe.patches.shared.misc.litho.filter.addLithoFilter
+import app.morphe.patches.shared.misc.settings.preference.InputType
+import app.morphe.patches.shared.misc.settings.preference.NonInteractivePreference
+import app.morphe.patches.shared.misc.settings.preference.PreferenceCategory
+import app.morphe.patches.shared.misc.settings.preference.PreferenceScreenPreference
+import app.morphe.patches.shared.misc.settings.preference.PreferenceScreenPreference.Sorting
+import app.morphe.patches.shared.misc.settings.preference.SwitchPreference
+import app.morphe.patches.shared.misc.settings.preference.TextPreference
+import app.morphe.patches.youtube.misc.litho.filter.lithoFilterPatch
+import app.morphe.patches.youtube.misc.proto.elementProtoParserHookPatch
+import app.morphe.patches.youtube.misc.settings.PreferenceScreen
+import app.morphe.patches.youtube.misc.settings.settingsPatch
+import app.morphe.patches.youtube.shared.Constants.COMPATIBILITY_YOUTUBE
+
+private const val BLOCK_CHANNELS_FILTER =
+    "Lapp/morphe/extension/youtube/patches/components/BlockChannelsFilter;"
+
+@Suppress("unused")
+val blockChannelsPatch = bytecodePatch(
+    name = "Block channels",
+    description = "Adds an option to permanently hide all content from specific channels " +
+            "(by name or @handle) in the Home feed, Subscriptions, Search results, related " +
+            "videos, Shorts shelves, and comments."
+) {
+    dependsOn(
+        lithoFilterPatch,
+        settingsPatch,
+        elementProtoParserHookPatch,
+        fixProtoLibraryPatch
+    )
+
+    compatibleWith(COMPATIBILITY_YOUTUBE)
+
+    execute {
+        PreferenceScreen.FEED.addPreferences(
+            PreferenceScreenPreference(
+                key = "morphe_block_channels_screen",
+                sorting = Sorting.UNSORTED,
+                preferences = setOf(
+                    SwitchPreference("morphe_block_channels"),
+                    TextPreference("morphe_block_channels_list", inputType = InputType.TEXT_MULTI_LINE),
+                    PreferenceCategory(
+                        key = "morphe_block_channels_about_category",
+                        titleKey = "morphe_hide_about_category_title",
+                        sorting = Sorting.UNSORTED,
+                        preferences = setOf(
+                            NonInteractivePreference(
+                                key = "morphe_block_channels_about",
+                                titleKey = "morphe_block_channels_screen_title",
+                                tag = "app.morphe.extension.shared.settings.preference.BulletPointPreference"
+                            )
+                        )
+                    )
+                )
+            )
+        )
+
+        addLithoFilter(BLOCK_CHANNELS_FILTER)
+    }
+}
